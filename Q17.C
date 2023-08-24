cead501df0313e4fea0cc0aa494d07bc9725709c
pavan99999999 <141390830+pavan99999999@users.noreply.github.com>
2023-08-24 09:56:15 +0530

    Create Q17.C

---
+++ Q17.C
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define BUFFER_SIZE 5
+#define NUM_PRODUCERS 2
+#define NUM_CONSUMERS 2
+
+int buffer[BUFFER_SIZE];
+int in = 0, out = 0;
+
+sem_t empty, full, mutex;
+
+void *producer(void *arg) {
+    int item;
+    for (int i = 0; i < BUFFER_SIZE * 2; i++) {
+        item = rand() % 100;  // Generate a random item
+        sem_wait(&empty);
+        sem_wait(&mutex);
+
+        buffer[in] = item;
+        printf("Produced: %d\n", item);
+        in = (in + 1) % BUFFER_SIZE;
+
+        sem_post(&mutex);
+        sem_post(&full);
+    }
+    pthread_exit(NULL);
+}
+
+void *consumer(void *arg) {
+    int item;
+    for (int i = 0; i < BUFFER_SIZE * 2; i++) {
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        item = buffer[out];
+        printf("Consumed: %d\n", item);
+        out = (out + 1) % BUFFER_SIZE;
+
+        sem_post(&mutex);
+        sem_post(&empty);
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    srand(time(NULL));
+
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+    pthread_t producers[NUM_PRODUCERS];
+    pthread_t consumers[NUM_CONSUMERS];
+
+    for (int i = 0; i < NUM_PRODUCERS; i++)
+        pthread_create(&producers[i], NULL, producer, NULL);
+
+    for (int i = 0; i < NUM_CONSUMERS; i++)
+        pthread_create(&consumers[i], NULL, consumer, NULL);
+
+    for (int i = 0; i < NUM_PRODUCERS; i++)
+        pthread_join(producers[i], NULL);
+
+    for (int i = 0; i < NUM_CONSUMERS; i++)
+        pthread_join(consumers[i], NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
+
+    return 0;
+}
